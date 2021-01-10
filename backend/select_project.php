d7a0ab5489047fc3122f1834d53236585064f86d
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2021-01-10 18:11:47 +0100

    dodanie innych rzeczy

--- backend/select_project.php
+++ backend/select_project.php
@@ -1,3 +1,34 @@
 <?php
+    include 'connect_db.php';
+    $dataJSON = file_get_contents("php://input");
+    $data = json_decode($dataJSON);
+
+    try {
+        $connection = connect_db();
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = $connection->prepare("SELECT id FROM submitted_projects WHERE email='".$data->email."';");
+        $query->execute();
+        $result = $query->setFetchMode(PDO::FETCH_ASSOC);
+        
+        $row = $query->fetch();
+        if ($row == NULL) {
+            try {
+                $connection = connect_db();
+                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $query = $connection->prepare("INSERT INTO submitted_projects (email,first_name,second_name,project_id) VALUES ('".$data->email."','".$data->first_name."','".$data->second_name."','".$data->project_id."');");
+                $query->execute();
+                echo '';
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+        }
+        else {
+            $error = "Aplikowałeś/aś już do jednego projektu.";
+            echo $error;
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $connection = null;
 
 ?>
