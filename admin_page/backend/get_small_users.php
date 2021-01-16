01d0d722ae389ef4ec5901c5a33a67e10d0c60d0
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2021-01-16 20:01:16 +0100

    tworzenie dodawania aplikacji
    Do dokończenia

---
+++ admin_page/backend/get_small_users.php
@@ -0,0 +1,27 @@
+<?php
+    include '../../backend/connect_db.php';
+    $json = array();
+    try {
+        $connection = connect_db();
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $blocked = "0";
+        $query = $connection->prepare("SELECT id, email FROM users WHERE blocked=:blocked;");
+        $query->bindParam(':blocked', $blocked);
+        $query->execute();
+        $result = $query->setFetchMode(PDO::FETCH_ASSOC);
+        $i = 0;
+        while ($row = $query->fetch()) {
+            $json[$i] = array(
+                "id" => $row['id'],
+                "email" => $row['email']
+            );
+            $i = $i + 1;
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $connection = null;
+    
+    $data = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    echo $data;
+?>
