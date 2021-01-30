31510b8dc17204723e0a5b3f61d09b925ea7cfe4
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2021-01-30 20:31:34 +0100

    Naprawienie menu aplikacji w panelu admina

--- admin_page/backend/get_small_users.php
+++ admin_page/backend/get_small_users.php
@@ -1,25 +1,58 @@
 <?php
     include '../../backend/connect_db.php';
-    $json = array();
+
     try {
+        //Pobranie wszystkich użytkowników
         $connection = connect_db();
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $query = $connection->prepare("SELECT id, email FROM users;");
         $query->execute();
         $result = $query->setFetchMode(PDO::FETCH_ASSOC);
+        $all_users = array();
         $i = 0;
         while ($row = $query->fetch()) {
-            $json[$i] = array(
+            $all_users[$i] = array(
                 "id" => $row['id'],
                 "email" => $row['email']
             );
             $i = $i + 1;
+        }
+        
+        //Pobranie użytkowników, którzy już aplikowali do projektu
+        $connection = connect_db();
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = $connection->prepare("SELECT email FROM submitted_projects;");
+        $query->execute();
+        $result = $query->setFetchMode(PDO::FETCH_ASSOC);
+        $submitted_users_emails = array();
+        $i = 0;
+        while ($row = $query->fetch()) {
+            $submitted_users_emails[$i] = $row['email'];
+            $i = $i + 1;
+        }
+
+        //Wyodrębnienie użytkowników, którzy jeszcze nie aplikowali do żadnego projektu
+        //tu jest błąd
+        $right_users = array();
+        $i = 0;
+        foreach ($all_users as $user) {
+            $flag = true;
+            foreach ($submitted_users_emails as $sub_user_email) {
+                if ($user['email'] == $sub_user_email) {
+                    $flag = false;
+                    break;
+                }
+            }
+            if ($flag) {
+                $right_users[$i] = $user;
+                $i = $i + 1;
+            }
         }
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
     $connection = null;
     
-    $data = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    $data = json_encode($right_users, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     echo $data;
 ?>
--- admin_page/backend/update_application.php
+++ admin_page/backend/update_application.php
@@ -6,13 +6,11 @@
     try {
         $connection = connect_db();
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $email = $data->email;
         $first_name = $data->first_name;
         $second_name = $data->second_name;
         $project_id = $data->project_id;
         $id = $data->id;
-        $query = $connection->prepare("UPDATE submitted_projects SET email=:email, first_name=:first_name, second_name=:second_name, project_id=:project_id WHERE id=:id;");
-        $query->bindParam(':email', $email);
+        $query = $connection->prepare("UPDATE submitted_projects SET first_name=:first_name, second_name=:second_name, project_id=:project_id WHERE id=:id;");
         $query->bindParam(':first_name', $first_name);
         $query->bindParam(':second_name', $second_name);
         $query->bindParam(':project_id', $project_id);
